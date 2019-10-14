21b7b1266cd259ab6540cbf81cf6a13e6a17388c
Antonio Kuzminski <kuzminski.antonio@gmail.com>
2019-10-14 14:04:33 +0200

    newton completed

--- zadaca2/src/hr/fer/zemris/numeric/Jednostavno.java
+++ zadaca2/src/hr/fer/zemris/numeric/Jednostavno.java
@@ -51,19 +51,65 @@ public class Jednostavno {
 			public double[][] hessian(double[] vector) {
 				return new double[][] {{2,0}, {0,2}};
 			}
+		};
+		
+		IFunction f2a = new IFunction() {
+
+			@Override
+			public int numOfVariables() {
+				return 2;
+			}
+
+			@Override
+			public double valueAt(double[] v) {
+				return Math.pow(v[0] - 1, 2) + 10 * Math.pow(v[1] - 2, 2);
+			}
+
+			@Override
+			public double[] gradient(double[] v) {
+				double[] gradient = new double[numOfVariables()];
+				gradient[0] = 2 * (v[0] - 1);
+				gradient[1] = 20 * (v[1] - 2);
+				return gradient; 
+			}
 			
-			
+		};
+		
+		IHFunction f2b = new IHFunction() {
+
+			@Override
+			public int numOfVariables() {
+				return 2;
+			}
+
+			@Override
+			public double valueAt(double[] v) {
+				return Math.pow(v[0] - 1, 2) + 10 * Math.pow(v[1] - 2, 2);
+			}
+
+			@Override
+			public double[] gradient(double[] v) {
+				double[] gradient = new double[numOfVariables()];
+				gradient[0] = 2 * (v[0] - 1);
+				gradient[1] = 20 * (v[1] - 2);
+				return gradient; 
+			}
+
+			@Override
+			public double[][] hessian(double[] vector) {
+				return new double[][] {{2,0}, {0,20}};
+			}
 		};
 		
 		DecimalFormat df = new DecimalFormat("#");
 		df.setMaximumFractionDigits(10);
 		
-		int maxIterations = 3;
+		int maxIterations = 50;
 		
 		
-		double[] sol = NumOptAlgorithms.newton(f1b, maxIterations);
+		double[] sol = NumOptAlgorithms.newton(f2b, maxIterations);
 		//System.out.println(initial[0] + " " + initial[1]);
-		System.out.println(sol[0] + " " + sol[1]);
+		
 		
 		System.out.print(df.format(sol[0]));
 		System.out.print(" ");
