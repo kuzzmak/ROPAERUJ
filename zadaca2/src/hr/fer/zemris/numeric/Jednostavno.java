88f45cd8eb12cc63bd2416669d23c267d9ac7295
Antonio Kuzminski <kuzminski.antonio@gmail.com>
2019-10-14 12:48:38 +0200

    newton tested

--- zadaca2/src/hr/fer/zemris/numeric/Jednostavno.java
+++ zadaca2/src/hr/fer/zemris/numeric/Jednostavno.java
@@ -1,6 +1,5 @@
 package hr.fer.zemris.numeric;
-
-import java.util.Random;
+import java.text.DecimalFormat;
 
 public class Jednostavno {
 
@@ -28,16 +27,51 @@ public class Jednostavno {
 			
 		};
 		
-		int maxIterations = 10000;
-		Random rand = new Random();
-		double low = -5;
-		double high = 5;
-		double[] initial = new double[] {low + (high - low) * rand.nextDouble(), low + (high - low) * rand.nextDouble()};
-		double[] sol = NumOptAlgorithms.gradientDescent(f1a, maxIterations, initial);
+		IHFunction f1b = new IHFunction() {
+
+			@Override
+			public int numOfVariables() {
+				return 2;
+			}
+
+			@Override
+			public double valueAt(double[] v) {
+				return Math.pow(v[0], 2) + Math.pow(v[1] - 1, 2);
+			}
+
+			@Override
+			public double[] gradient(double[] v) {
+				double[] gradient = new double[numOfVariables()];
+				gradient[0] = 2 * v[0];
+				gradient[1] = 2 * (v[1] - 1);
+				return gradient;
+			}
+
+			@Override
+			public double[][] hessian(double[] vector) {
+				return new double[][] {{2,0}, {0,2}};
+			}
+			
+			
+		};
+		
+		DecimalFormat df = new DecimalFormat("#");
+		df.setMaximumFractionDigits(10);
+		
+		int maxIterations = 3;
+		
+		
+		double[] sol = NumOptAlgorithms.newton(f1b, maxIterations);
 		//System.out.println(initial[0] + " " + initial[1]);
 		System.out.println(sol[0] + " " + sol[1]);
 		
+		System.out.print(df.format(sol[0]));
+		System.out.print(" ");
+		System.out.print(df.format(sol[1]));
+		System.out.println();
+		
 	}
+	
 	
 	
 	
