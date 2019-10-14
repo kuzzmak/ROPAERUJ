6b669b8b4ccd375f0aefd747439dd501e3adaa40
Antonio Kuzminski <kuzminski.antonio@gmail.com>
2019-10-14 17:58:45 +0200

    added graph visualization

--- zadaca2/src/hr/fer/zemris/numeric/Jednostavno.java
+++ zadaca2/src/hr/fer/zemris/numeric/Jednostavno.java
@@ -1,9 +1,33 @@
 package hr.fer.zemris.numeric;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jfree.ui.RefineryUtilities;
 
 public class Jednostavno {
 
-	public static void main(String[] args) {
+	public static List<double[]> getGraphData(String fileName){
+		List<double[]> graphData = new ArrayList<>();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(fileName));) {
+		    String line = br.readLine();
+		    
+		    while (line != null) {
+		    	String[] temp = line.split(",");
+		    	graphData.add(new double[] {Double.parseDouble(temp[0]), Double.parseDouble(temp[1])});
+		    	line = br.readLine();
+		    }
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return graphData;
+	}
+	
+	public static void main(String[] args) throws IOException {
 		
 		IFunction f1a = new IFunction() {
 
@@ -104,13 +128,19 @@ public class Jednostavno {
 		DecimalFormat df = new DecimalFormat("#");
 		df.setMaximumFractionDigits(10);
 		
-		int maxIterations = 50;
+		int maxIterations = 1000;
 		
 		
-		double[] sol = NumOptAlgorithms.newton(f2b, maxIterations);
-		//System.out.println(initial[0] + " " + initial[1]);
+		double[] sol = NumOptAlgorithms.gradientDescent(f1a, maxIterations);
 		
-		
+		List<double[]> graphData = getGraphData("graphData");
+		for(int i = 0; i < graphData.size(); i++) {
+			System.out.println(graphData.get(i)[0] + " " + graphData.get(i)[1]);
+		}
+		final Graph demo = new Graph(graphData, "XY Series Demo");
+		demo.pack();
+		RefineryUtilities.centerFrameOnScreen(demo);
+		demo.setVisible(true);
 		System.out.print(df.format(sol[0]));
 		System.out.print(" ");
 		System.out.print(df.format(sol[1]));
