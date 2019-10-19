a0d2ac31e2617e59b81155f14a23e65281b603c7
Antonio Kuzminski <kuzminski.antonio@gmail.com>
2019-10-19 10:35:37 +0200

    BitVectorDecoder completed

--- zadaca3/src/hr/fer/zemris/optjava/dz3/BitVectorDecoder.java
+++ zadaca3/src/hr/fer/zemris/optjava/dz3/BitVectorDecoder.java
@@ -1,25 +1,42 @@
 package hr.fer.zemris.optjava.dz3;
 
-public class BitVectorDecoder implements IDecoder<SingleObjectiveSolution>{
+import java.util.Arrays;
 
-	double[] mins;
-	double[] maxs;
-	int[] bits;
-	int n;
-	int totalBits;
-	
-	
-	
-	@Override
-	public double[] decode(SingleObjectiveSolution obj) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public abstract class BitVectorDecoder implements IDecoder<SingleObjectiveSolution>{
 
-	@Override
-	public void decode(double[] v, SingleObjectiveSolution obj) {
-		// TODO Auto-generated method stub
+	private double[] mins;
+	private double[] maxs;
+	private int[] bits;
+	private int n;
+	private int totalBits = 0;
+	
+	public BitVectorDecoder(double[] mins, double[] maxs, int[] bits, int n) {
+		this.mins = mins;
+		this.maxs = maxs;
+		this.bits = bits;
+		this.n = n;
 		
+		for(int i = 0; i < bits.length; i++) {
+			totalBits += bits[i];
+		}
+	}
+	
+	public BitVectorDecoder(double min, double max, int nBits, int n) {
+		this.n = n;
+		this.mins = new double[n];
+		this.maxs = new double[n];
+		this.bits = new int[n];
+		Arrays.fill(this.mins, min);
+		Arrays.fill(this.maxs, max);
+		Arrays.fill(this.bits, nBits);
 	}
 
+	public int getTotalBits() {
+		return totalBits;
+	}
+	
+	public int getDimensions() {
+		return n;
+	}
+	
 }
