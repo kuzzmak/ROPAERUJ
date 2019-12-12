7e6fa562e72a161b60f26043ed3bb2236e714cec
Antonio Kuzminski <kuzminski.antonio@gmail.com>
2019-12-13 00:39:47 +0100

    added new activation function

---
+++ zadaca8/src/hr/fer/zemris/optjava/dz8/NN/TangentHyperbolicLayer.java
@@ -0,0 +1,33 @@
+package hr.fer.zemris.optjava.dz8.NN;
+
+import java.util.Arrays;
+
+public class TangentHyperbolicLayer implements NeuralNetLayer {
+
+	Neuron[] neurons;
+	
+	public TangentHyperbolicLayer(Neuron[] neurons) {
+		this.neurons = neurons;
+	}
+	
+	@Override
+	public int getNumOfNeurons() {
+		return neurons.length;
+	}
+
+	@Override
+	public Neuron[] getNeurons() {
+		return neurons;
+	}
+
+	@Override
+	public IActivationFunction getActivationFunction() {
+		return new TangentHyperbolic();
+	}
+
+	@Override
+	public String toString() {
+		return "TangentHyperbolicLayer [neurons=" + Arrays.toString(neurons) + "]";
+	}
+	
+}
