14e7a2752c3ecfbce784074fa8f95778efa6c0f4
Antonio Kuzminski <kuzminski.antonio@gmail.com>
2020-01-17 19:59:58 +0100

    update

--- zadaca11/src/hr/fer/zemris/optjava/rng/provimpl/ThreadLocalRNGProvider.java
+++ zadaca11/src/hr/fer/zemris/optjava/rng/provimpl/ThreadLocalRNGProvider.java
@@ -2,14 +2,15 @@ package hr.fer.zemris.optjava.rng.provimpl;
 
 import hr.fer.zemris.optjava.rng.IRNG;
 import hr.fer.zemris.optjava.rng.IRNGProvider;
+import hr.fer.zemris.optjava.rng.RNG;
 
 public class ThreadLocalRNGProvider implements IRNGProvider{
 
-	private ThreadLocal<IRNG> threadLocal = new ThreadLocal<>();
+	private ThreadLocal<IRNG> threadLocal = ThreadLocal.withInitial(() -> {return RNG.getRNG();}); 
 	
 	@Override
 	public IRNG getRNG() {
-		return 
+		return threadLocal.get();
 	}
 
 }
