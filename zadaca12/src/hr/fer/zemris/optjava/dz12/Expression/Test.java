c92ca28045d961fe833ef57b0b42cd610cd21f5e
Antonio Kuzminski <kuzminski.antonio@gmail.com>
2020-01-25 13:53:15 +0100

    update

--- zadaca12/src/hr/fer/zemris/optjava/dz12/Expression/Test.java
+++ zadaca12/src/hr/fer/zemris/optjava/dz12/Expression/Test.java
@@ -24,7 +24,7 @@ public class Test {
 		terminals.add(new Terminal("Left", Action.LEFT));
 		terminals.add(new Terminal("Move", Action.MOVE));
 
-		makeTree(10);
+		makeTree(3);
 
 //		IExpression iff = new IF();
 //		((IF) iff).addOutput(new IF());
@@ -48,28 +48,56 @@ public class Test {
 		IFunction fun = (IFunction) node.getUserObject();
 		// za svako dijete se stvara neki izraz
 		for (int i = 0; i < fun.getNumberOfOutputs(); i++) {
+			
+			// zadnja dozvoljena razina
+			if(depth == 0) {
+				
+				Expression e = terminals.get(rand.nextInt(terminals.size())).duplicate();
+				fun.addOutput(e);
+				
+			}else {
+				// ako je moguce dodati jos izraza
+				if (fun.canAdd()) {
 
-			// ako je moguce dodati jos izraza
-			if (fun.canAdd()) {
-
-				// nasumican izbor izraza
-				if (rand.nextFloat() > 0.5) {
-					Expression e = terminals.get(rand.nextInt(terminals.size())).duplicate();
-					fun.addOutput(e);
-				} else {
-					Expression e = functions.get(rand.nextInt(functions.size())).duplicate();
-					fun.addOutput(e);
+					// nasumican izbor izraza
+					if (rand.nextFloat() > 0.5) {
+						Expression e = terminals.get(rand.nextInt(terminals.size())).duplicate();
+						fun.addOutput(e);
+					} else {
+						Expression e = functions.get(rand.nextInt(functions.size())).duplicate();
+						fun.addOutput(e);
+					}
 				}
+				
+				
 			}
+
+			
 		}
-		
+
 		DefaultMutableTreeNode result = new DefaultMutableTreeNode(fun);
-		for(Expression e: fun.getOutputs()) {
+		for (Expression e : fun.getOutputs()) {
 			node.add(new DefaultMutableTreeNode(e));
 		}
-		 
-//		return result;
+
+		Enumeration<DefaultMutableTreeNode> en = node.children();
 		
+		while (en.hasMoreElements()) {
+			
+			DefaultMutableTreeNode child = en.nextElement();
+			
+			exp = (Expression) child.getUserObject();
+			
+			if(exp.status == Status.FUNCTION) {
+				
+				fun = (IFunction) child.getUserObject();
+				
+				if(fun.canAdd()) {
+					generateExpressions(child, depth - 1);
+				}
+			}
+			
+		}
 	}
 
 	public static void makeTree(int depth) {
@@ -77,23 +105,25 @@ public class Test {
 		Expression root = functions.get(rand.nextInt(functions.size()));
 
 		DefaultMutableTreeNode rootnode = new DefaultMutableTreeNode(root);
-		
+
 		System.out.println("root\n" + rootnode.getUserObject());
 		generateExpressions(rootnode, depth);
 
 		System.out.println("children");
 		Enumeration<DefaultMutableTreeNode> en = rootnode.children();
+//		while (en.hasMoreElements()) {
+//			generateExpressions(en.nextElement(), 10);
+//		}
+
+		en = rootnode.children();
 		while (en.hasMoreElements()) {
 			System.out.println(en.nextElement());
 		}
-		
+
 //		PR3 pr31 = new PR3();
 //		pr31.addOutput(new IF());
 //		PR3 pr32 = (PR3)pr31.duplicate();
 //		PR3 pr33 = (PR3)pr31.duplicate();
-		
-		
-
 
 //		while (rootnode.getDepth() < 5) {
 //
